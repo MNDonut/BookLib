fad242cb3f308a0d3ada33efb13c777d31d9ef4d
Ihor <mazahaka1982@gmail.com>
2021-10-30 18:12:46 +0300

    Added books on the edition page

--- author/views.py
+++ author/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+
+from book.models import Book
 from .models import Author
 
 def author(request, slug):
     author = Author.objects.get(slug=slug)
+    books = Book.objects.filter(author__slug=slug)
     context = {
-        'author': author
+        'author': author,
+        'books': books
     }
     return render(request, 'author.html', context)
 
--- edition/views.py
+++ edition/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+
+from book.models import Book
 from .models import Edition
 
 def edition(request, slug):
     edition = Edition.objects.get(slug=slug)
+    books = Book.objects.filter(edition__slug=slug)
     context = {
-        'edition': edition
+        'edition': edition,
+        'books': books
     }
     return render(request, 'edition.html', context)
 
